788cb1ef48c33053b886f997aa4ae4c902e52c84
01010110 <f4rei8ht@gmail.com>
2025-03-19 02:12:13 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-    // Показать кнопку, если прокрутка больше 300px
     window.addEventListener("scroll", function () {
         if (window.scrollY > 300) {
             scrollToTopBtn.classList.add("show");
@@ -13,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Плавная прокрутка наверх
     scrollToTopBtn.addEventListener("click", function () {
         window.scrollTo({ top: 0, behavior: "smooth" });
     });

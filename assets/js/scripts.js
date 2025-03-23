0fee3a0c6ead39e098fbdab4b86c6d6acb2dd440
01010110 <f4rei8ht@gmail.com>
2025-03-23 17:17:52 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 300) {
         scrollToTopBtn.classList.add("show");
     } else {
         scrollToTopBtn.classList.remove("show");
